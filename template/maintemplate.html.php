a6293705d8d7d2737a5fc218213ee6eff2e627bd
lizward <47131253+lizward@users.noreply.github.com>
2019-02-12 20:34:07 -0800

    v3

--- template/maintemplate.html.php
+++ template/maintemplate.html.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     
-    <title>Template</title>
+    <title>Main Template</title>
     
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -38,15 +38,11 @@
                 
             </aside>
 
-         <section class="form">
+         <section>
              
-            <form>
-                
-                <h2>Heading 2 (h2)</h2>
-                
-                <p>Form goes here</p>
-                
-            </form>
+            <h2>Heading 2 (h2)</h2>
+
+             <p>Link to Registration form goes here</p>
              
          </section>
 
